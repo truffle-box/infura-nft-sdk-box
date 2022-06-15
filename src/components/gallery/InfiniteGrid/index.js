40e9601047a359adce11844939cdbbcc4c8d1224
MB <951378+michaeljohnbennett@users.noreply.github.com>
2022-06-15 17:10:42 +1000

    Rationalising the grid view.
    Adding in video/svg and image loaders based on axios mimetype gets. It will slow it down but it makes it almost foolproof.
    
    Still need to catch the odd null.
    
    Just need to add in MP3 tomorrow and its done.

--- src/components/gallery/InfiniteGrid/index.js
+++ src/components/gallery/InfiniteGrid/index.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useCallback, useEffect, useState } from "react";
 
 import { MasonryInfiniteGrid } from "@egjs/react-infinitegrid";
 import { MOCK_DATA } from "../mocksdk";
-import './index.css'
+import axios from "axios";
+import { PuffLoader } from "react-spinners";
+
+import "./index.css";
 
 function getItems(nextGroupKey, count) {
   const nextItems = [];
@@ -22,30 +25,106 @@ function getItems(nextGroupKey, count) {
   return nextItems;
 }
 
-const Item = ({ num, asset }) => <div className="item">
-  <div className="thumbnail">
-    <NFTImage url={asset?.metadata?.image} />
-  </div>
-  <div className="info">
-    <span>Name: {asset?.metadata?.name}</span><br />
-    <span className={"description_text"}>Desc: {asset?.metadata?.description}</span>
-  </div>
-</div>;
+const Item = ({ num, asset }) =>
+  <Suspense fallback={<PuffLoader loading={true} />}>
+    <div className="item">
+      <div className="thumbnail">
+        <NFTImage url={asset?.metadata?.image} />
+      </div>
+      <div className="info">
+        <div className="title">Name: {asset?.metadata?.name}</div>
+        <br />
+        <div className="description">Desc: {asset?.metadata?.description}</div>
+      </div>
+    </div>
+  </Suspense>;
 
-const NFTImage = ({ url = "" }) => {
-  const [realUrl, setRealUrl] = useState(url);
+const RenderNFTImage = ({ url, mimeType }) => {
+
+  const [isImage, setIsImage] = useState(false);
+  const [isVideo, setIsVideo] = useState(false);
+  const [isSvg, setIsSvg] = useState(false);
+
   useEffect(() => {
-    if (url.toLowerCase().startsWith("ipfs://")) {
-      const hash = url.split("//")[1];
-      setRealUrl(`https://ipfs.io/ipfs/${hash}`);
-      console.log("RealUrl", { url, realUrl });
+    // set the hoobmajoob
+    if (mimeType.startsWith("image")) {
+      setIsImage(true);
+      setIsVideo(false);
+      setIsSvg(false);
     }
-  }, [url]);
+    if (mimeType.startsWith("video")) {
+      setIsImage(false);
+      setIsVideo(true);
+      setIsSvg(false);
+    }
+    console.log("Rendering Image: ", { isImage, isVideo, isSvg, url, mimeType });
+  }, [url, mimeType]);
 
   return (
-    <img
-      src={realUrl}
-      alt={realUrl} />
+    <>
+      {isImage && <img src={url} />}
+      {isSvg && <div
+        className="svgImage"
+        style={{
+          background: url,
+          width: "100%",
+          height: 500
+        }}>SVG</div>
+      }
+      {isVideo && <video className="videoThumbnail" src={url} loop={true} autoPlay={true} controls={true} />}
+      {!isImage && !isVideo && !isSvg && <>NO IMAGE</>}
+    </>
+  );
+};
+
+const NFTImage = ({ url = "" }) => {
+  const regexp = /^data:(?<mimetype>.*?);/;
+  const [realUrl, setRealUrl] = useState(url);
+  const [mimeType, setMimeType] = useState("unknown");
+  useEffect(() => {
+    let newUrl = realUrl;
+    // grab the mime type out of the core svg value.
+    if (realUrl.startsWith("data:image")) {
+      const groups = realUrl.match(regexp);
+      if (groups.length === 2) {
+        // console.log("mediatype matches: ", { realUrl, groups, g: groups[1] });
+        setMimeType(groups[1]);
+      }
+      // at this point we don't need to do any url gets/mimetypes so we fail out.
+      return;
+    }
+    if (realUrl.toLowerCase().startsWith("ipfs://")) {
+      let bits = url.split("ipfs://");
+      if (bits[1].startsWith("ipfs/")) {
+        // this is a sickening hack but we are dealing with metadata that is rubbish.
+        bits = bits[1].split("ipfs/");
+      }
+      newUrl = `https://ipfs.io/ipfs/${bits[1]}`;
+      setRealUrl(newUrl);
+      // console.log("RealUrl", { url, realUrl, newUrl });
+    }
+    processUrlHead(newUrl).catch(e => {
+      console.log("Error: ", e);
+    });
+  }, [url]);
+
+  const processUrlHead = async (headUrl) => axios.head(headUrl)
+                                                 .then((h) => {
+                                                   setMimeType(h.headers["content-type"]);
+                                                   // console.log("HeadVals: ", { headUrl, h });
+                                                   return h;
+                                                 }, (reason) => {
+                                                   console.log("Error:", { headUrl, reason });
+                                                 });
+
+  const renderCallback = useCallback(() => {
+    return <RenderNFTImage url={realUrl} mimeType={mimeType} />;
+  }, [realUrl, mimeType]);
+
+  return (
+    <div className="image">
+      {renderCallback()}
+    </div>
   );
 };
 
@@ -54,11 +133,8 @@ const GalleryView = () => {
 
   return <MasonryInfiniteGrid
     className="container"
-    gap={5}
+    gap={10}
     align={"justify"}
-    attributePrefix={"p"}
-    isConstantSize={true}
-    isEqualSize={true}
     onRequestAppend={(e) => {
       const nextGroupKey = (+e.groupKey || 0) + 1;
       console.log("onRequestAppend:", { e, nextGroupKey, items });
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,45 +1,41 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
-import { EthProvider } from "../ethereum";
-import { MOCK_DATA } from "../components/gallery/mocksdk";
+import React from "react";
 import GalleryView from "../components/gallery/InfiniteGrid";
 
 const Gallery = () => {
-  const { provider, sdk, user } = useContext(EthProvider);
-  const { address } = user;
-  const [nfts, setNfts] = useState([]);
-
-  const checkConnection = () => {
-    switch (true) {
-      case sdk && address !== "":
-        return <h1>Your NFTs</h1>;
-      case provider !== null:
-        return <h3>Connect your wallet to view your NFT gallery</h3>;
-      case !provider:
-      default:
-        return <h3>You will need to install MetaMask to use this application</h3>;
-    }
-  };
-
-  const listNFTs = useCallback(async () => {
-    try {
-      const data = MOCK_DATA;
-      setNfts(data.assets);
-    } catch (e) {
-      console.log(e);
-    }
-  }, [sdk]);
-
-  useEffect(() => {
-    if (sdk) {
-      listNFTs();
-    }
-  });
+  // const { provider, sdk, user } = useContext(EthProvider);
+  // const { address } = user;
+  // const [nfts, setNfts] = useState([]);
+  //
+  // const checkConnection = () => {
+  //   switch (true) {
+  //     case sdk && address !== "":
+  //       return <h1>Your NFTs</h1>;
+  //     case provider !== null:
+  //       return <h3>Connect your wallet to view your NFT gallery</h3>;
+  //     case !provider:
+  //     default:
+  //       return <h3>You will need to install MetaMask to use this application</h3>;
+  //   }
+  // };
+  //
+  // const listNFTs = useCallback(async () => {
+  //   try {
+  //     const data = MOCK_DATA;
+  //     setNfts(data.assets);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }, [sdk]);
+  //
+  // useEffect(() => {
+  //   if (sdk) {
+  //     listNFTs();
+  //   }
+  // });
 
   return (
     <>
       <GalleryView />
-      {/*  <AlternateGallery/>*/}
-      {/*  <ProGallery/>*/}
     </>
   );
 };
